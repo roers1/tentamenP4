6bf603ff960c01929700e3b5394b815ca057c4e5
Ruben van Oers <r.vanoers1@student.avans.nl>
2019-05-09 19:33:17 +0200

    added: error catch

--- app.js
+++ app.js
@@ -16,4 +16,20 @@ app.all('*', (req, res, next) => {
   //routes bepalen en doorlinken naar het juiste route bestand
 app.use('/api/appartments', appartmentsRoute)
 
+app.use('*', (req, res, next) => {
+    const error = new Error('Route not found');
+    error.status = (404);
+    next(error);
+  })
+
+//Hier worden de errors in opgevangen, ook een database error kan hierin terecht komen
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+      error: {
+        message: error.message
+      }
+    })
+  })
+
   module.exports = app;
