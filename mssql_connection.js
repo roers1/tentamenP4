ba07518c0907ebd2345adfe5441a57d49f182aea
Ruben van Oers <r.vanoers1@student.avans.nl>
2019-05-11 12:39:11 +0200

    added: mssql connection file

---
+++ mssql_connection.js
@@ -0,0 +1,43 @@
+//mssql-connection
+const mssql = require('mssql');
+const config = require('./config/config')
+
+const logger = config.logger
+//config gegevens voor de database
+const dbConfig = config.dbConfig
+
+function executeQuery(query, callback) {
+  logger.info(query)
+  var conn = new mssql.ConnectionPool(dbConfig);
+  var req = new mssql.Request(conn);
+
+  conn.connect((err) => {
+    if (err) {
+      logger.error('error', err);
+      return;
+    }
+
+    req.query(query, (err, recordset) => {
+      if (err) {
+        logger.error('error', err);
+        callback(err, null, null)
+      }
+
+      //recordset.recordset is van toepassing op GET functies
+      //recordset.rowsaffected is van toepassing op INSERT, UPDATE, DELETE functies
+      if (recordset.recordset) {
+        logger.info(recordset);
+        callback(null, recordset.recordset)
+      } else {
+        logger.info(recordset.rowsAffected);
+        callback(null, recordset.rowsAffected)
+      }
+      conn.close();
+    })
+  })
+
+}
+
+module.exports = {
+  executeQuery
+}
