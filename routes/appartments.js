9ecb7f702e823162e492cc3b1ff2ef04531b29c0
Ruben van Oers <r.vanoers1@student.avans.nl>
2019-05-11 13:22:48 +0200

    added: get:Id for /appartments route

--- routes/appartments.js
+++ routes/appartments.js
@@ -11,6 +11,7 @@ router.get('/', (req, res, next) => {
     //Query welke uitgevoerd gaat worden door de database
     const query = ('SELECT * FROM Apartment ' +
         'JOIN Reservation ON [Apartment].[ApartmentId] = [Reservation].[ApartmentId] ' +
+        'JOIN DBUser ON Reservation.UserId = DBUser.UserId ' +
         'WHERE [Status] = \'ACCEPTED\';')
 
     logger.info('Used query is:' + query);
@@ -41,14 +42,34 @@ router.post('/', (req, res, next) => {
 })
 
 router.get('/:id', (req, res, next) => {
-    res.status(200).json({
-        message: 'GET request naar /api/appartments/:id'
-    })
-})
+    logger.info('Handling GET request to /api/appartments')
 
-router.put('/:id', (req, res, next) => {
-    res.status(200).json({
-        message: 'PUT request naar /api/appartments/:id'
+    const apartmentId = req.params.id;
+
+    //Query welke uitgevoerd gaat worden door de database
+    const query = ('SELECT * FROM Apartment ' +
+        'JOIN Reservation ON [Apartment].[ApartmentId] = [Reservation].[ApartmentId] ' +
+        'JOIN DBUser ON Reservation.UserId = DBUser.UserId ' +
+        'WHERE Apartment.ApartmentId = ' + apartmentId + ';')
+
+    logger.info('Used query is:' + query);
+
+    database.executeQuery(query, (err, rows) => {
+        //Als de database een error verstuurd zal de error doorgegeven worden naar de gebruiker
+        if (err) {
+            const error = {
+                message: err,
+                code: 500
+            }
+            next(error)
+        }
+
+        //Als er geen error is worden de rijen getoont die uit de query volgen
+        if (rows) {
+            res.status(200).json({
+                result: rows
+            })
+        }
     })
 })
 
