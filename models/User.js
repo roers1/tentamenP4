e83d5d419e7a578c7adc5b775a640d9b531db149
Ruben van Oers <r.vanoers1@student.avans.nl>
2019-05-11 12:28:35 +0200

    added: REGEX in models

--- models/User.js
+++ models/User.js
@@ -1,14 +1,44 @@
 class User{
 
-    constructor(FirstName, LastName, StreetAddress, PostcalCode, City, DateOfBirth, PhoneNumber, EmailAddress, Password){
+    constructor(FirstName, LastName, StreetAddress, PostalCode, City, DateOfBirth, PhoneNumber, EmailAddress, Password){
         this.FirstName = FirstName;
         this.LastName = LastName;
         this.StreetAddress = StreetAddress;
-        this.PostcalCode = PostcalCode;
+        this.PostalCode = this.validatePostalCode(PostalCode);
         this.City = City;
         this.DateOfBirth = DateOfBirth;
-        this.PhoneNumber = PhoneNumber;
-        this.EmailAddress = EmailAddress;
+        this.PhoneNumber = this.validatePhoneNumber(PhoneNumber);
+        this.EmailAddress = this.validateEmail(EmailAddress);
         this.Password = Password;
     }
+
+    validatePhoneNumber(PhoneNumber){
+            const regex = /(^(316|06|6)([0-9]{8}))$/
+            if (regex.test(PhoneNumber)) {
+                return PhoneNumber;
+              } else {
+                throw new Error("Invalid PhoneNumber: " + PhoneNumber.substring(0, 50))
+              }
+    }
+
+    validatePostalCode(PostalCode){
+        const regex = /(^[1-9][0-9]{3})([\s]?)((?!sa|sd|ss|SA|SD|SS)[A-Za-z]{2})$/
+    if (regex.test(postcode)) {
+      return postcode;
+    } else {
+      throw new Error("Invalid postcode: " + postcode.substring(0, 50))
+    }
+    }
+
+    validateEmail(email) {
+        const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (regex.test(email)) {
+          return email;
+        } else {
+          throw new Error("Invalid email: " + email.substring(0, 100));
+        }
+    
+      }
 }
+
+module.exports = User;
--- models/appartment.js
+++ models/appartment.js
@@ -3,8 +3,19 @@ class appartment{
     constructor(Description, StreetAddress, PostalCode, City, UserId){
         this.Description = Description;
         this.StreetAddress = StreetAddress;
-        this.PostalCode = PostalCode;
+        this.PostalCode = this.validatePostalCode(PostalCode);
         this.City = City;
         this.UserId = UserId;
     }
+
+
+    validatePostalCode(PostalCode){
+        const regex = /(^[1-9][0-9]{3})([\s]?)((?!sa|sd|ss|SA|SD|SS)[A-Za-z]{2})$/
+    if (regex.test(postcode)) {
+      return postcode;
+    } else {
+      throw new Error("Invalid postcode: " + postcode.substring(0, 50))
+    }
+    }
+
 }
