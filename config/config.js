a53e2e92c80790545570e7e7cf1aa22e35b69762
Ruben van Oers <r.vanoers1@student.avans.nl>
2019-05-11 12:30:16 +0200

    Added: config for local database

--- config/config.js
+++ config/config.js
@@ -3,5 +3,19 @@ module.exports = {
       root: './logs',
       maxLogFiles: 10,
       allLogsFileName: 'films_en_gebruikers'
-    })
+    }),
+
+    dbConfig: {
+      server: 'localhost\\MSSQLSERVER',
+      database: 'movieApplication',
+      user: 'sa',
+      password: 'WwvSQL123',
+      port: 1433,
+      driver: 'msnodesql',
+      connectionTimeout: 1500,
+      options: {
+        // 'true' if you're on Windows Azure
+        encrypt: false
+      }
+    }
 }
