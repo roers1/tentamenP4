011597ac467e49f8bfb830d83563a6b55797c4c8
Ruben van Oers <r.vanoers1@student.avans.nl>
2019-05-11 12:44:59 +0200

    added: SQL query for GET /api/appartments

--- routes/appartments.js
+++ routes/appartments.js
@@ -3,12 +3,34 @@ const router = express.Router();
 const config = require('../config/config');
 const logger = config.logger;
 
-router.get('/',(req,res,next)=>{
+var database = require('../mssql_connection')
 
-    //SELECT * FROM Apartment
-    //JOIN Reservation ON Apartment.ApartmentId = Reservation.ApartmentId
-    //WHERE [Status] = 'ACCEPTED';
-    
+router.get('/',(req,res,next)=>{
+    logger.info('Handling GET request to /api/appartments')
+
+    //Query welke uitgevoerd gaat worden door de database
+    const query = 'SELECT * FROM Apartment' +
+    'JOIN Reservation ON Apartment.ApartmentId = Reservation.ApartmentId'+
+    'WHERE [Status] = \'ACCEPTED\';'
+
+    database.executeQuery(query,(err,rows) =>{
+        //Als de database een error verstuurd zal de error doorgegeven worden naar de gebruiker
+        if (err) {
+            const error = {
+              message: err,
+              code: 500
+            }
+            next(error)
+          }
+      
+          //Als er geen error is worden de rijen getoont die uit de query volgen
+          if (rows) {
+            res.status(200).json({
+              result: rows
+            })
+          }
+    })
+
     res.status(200).json({
         message: 'GET request naar /api/appartments'
     })
