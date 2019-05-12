ac49dd9a89ff0ad05b74e65ec94fc8b6849023e7
Ruben van Oers <r.vanoers1@student.avans.nl>
2019-05-12 20:18:10 +0200

    improved: All request types with AUTH

--- routes/appartments.js
+++ routes/appartments.js
@@ -12,14 +12,45 @@ router.get('/', checkAuth,(req, res, next) => {
     logger.info('Handling GET request to /api/appartments')
 
     //Query welke uitgevoerd gaat worden door de database
-    const query = ('SELECT * FROM Apartment ' +
-        'JOIN Reservation ON [Apartment].[ApartmentId] = [Reservation].[ApartmentId] ' +
-        'JOIN DBUser ON Reservation.UserId = DBUser.UserId ' +
-        'WHERE [Status] = \'ACCEPTED\';')
+    const query = ('SELECT	[Apartment].[ApartmentId]' +
+		',[Apartment].[Description]'+
+		',[Apartment].[PostalCode]'+
+		',[Apartment].[StreetAddress]'+
+		',[DBUser].[UserId] AS OwnerId'+
+		',[DBUser].[FirstName] AS OwnerFirstName'+
+		',[DBUser].[LastName] AS OwnerLastName'+
+		',[DBUser].[StreetAddress] AS OwnerStreetAddress'+
+		',[DBUser].[PostalCode] AS OwnerPostalCode'+
+		',[DBUser].[City] AS OwnerCity'+
+		',[DBUser].[DateOfBirth] AS OwnerDateOfBirth'+
+		',[DBUser].[PhoneNumber] AS OwnerPhoneNumber'+
+		',[DBUser].[EmailAddress] AS OwnerEmailAddress'+
+		',[Reservation].[ReservationId]'+
+		',[Reservation].[StartDate] AS ReservationStartDate'+
+		',[Reservation].[EndDate] AS ReservationEndDate'+
+		',[Reservation].[Status] AS ReservationStatus'+
+		',[Reservation].[UserId] AS ReservationUserId '+
+'FROM Apartment JOIN Reservation ON [Apartment].[ApartmentId] = [Reservation].[ApartmentId] JOIN DBUser ON Reservation.UserId = DBUser.UserId WHERE [Status] = \'ACCEPTED\'')
 
     logger.info('Used query is:' + query);
 
-    executeQuery(query, req, res, next)
+    database.executeQuery(query, (err, rows) => {
+        //Als de database een error verstuurd zal de error doorgegeven worden naar de gebruiker
+        if (err) {
+            const error = {
+                message: err,
+                code: 500
+            }
+            next(error)
+        }
+
+        //Als er geen error is worden de rijen getoont die uit de query volgen
+        if (rows) {
+            res.status(200).json({
+                result: rows
+            })
+        }
+    })
 
 })
 
@@ -51,7 +82,23 @@ router.post('/', checkAuth,(req, res, next) => {
 
         logger.info('query: ' + query)
 
-        executeQuery(query, req, res, next)
+        database.executeQuery(query, (err, rows) => {
+            //Als de database een error verstuurd zal de error doorgegeven worden naar de gebruiker
+            if (err) {
+                const error = {
+                    message: err,
+                    code: 500
+                }
+                next(error)
+            }
+    
+            //Als er geen error is worden de rijen getoont die uit de query volgen
+            if (rows) {
+                res.status(200).json({
+                    result: rows
+                })
+            }
+        })
 
     } catch (ex) {
         next(ex);
@@ -65,17 +112,52 @@ router.get('/:id',checkAuth, (req, res, next) => {
     const apartmentId = req.params.id;
 
     //Query welke uitgevoerd gaat worden door de database
-    const query = ('SELECT * FROM Apartment ' +
-        'JOIN Reservation ON [Apartment].[ApartmentId] = [Reservation].[ApartmentId] ' +
-        'JOIN DBUser ON Reservation.UserId = DBUser.UserId ' +
+    const query = ('SELECT	[Apartment].[ApartmentId]' +
+		',[Apartment].[Description]'+
+		',[Apartment].[PostalCode]'+
+		',[Apartment].[StreetAddress]'+
+		',[DBUser].[UserId] AS OwnerId'+
+		',[DBUser].[FirstName] AS OwnerFirstName'+
+		',[DBUser].[LastName] AS OwnerLastName'+
+		',[DBUser].[StreetAddress] AS OwnerStreetAddress'+
+		',[DBUser].[PostalCode] AS OwnerPostalCode'+
+		',[DBUser].[City] AS OwnerCity'+
+		',[DBUser].[DateOfBirth] AS OwnerDateOfBirth'+
+		',[DBUser].[PhoneNumber] AS OwnerPhoneNumber'+
+		',[DBUser].[EmailAddress] AS OwnerEmailAddress'+
+		',[Reservation].[ReservationId]'+
+		',[Reservation].[StartDate] AS ReservationStartDate'+
+		',[Reservation].[EndDate] AS ReservationEndDate'+
+		',[Reservation].[Status] AS ReservationStatus'+
+		',[Reservation].[UserId] AS ReservationUserId '+
+        'FROM Apartment '+
+        'JOIN Reservation ON [Apartment].[ApartmentId] = [Reservation].[ApartmentId] '+
+        'JOIN DBUser ON Reservation.UserId = DBUser.UserId '+
         'WHERE Apartment.ApartmentId = ' + apartmentId + ';')
 
     logger.info('Used query is:' + query);
 
-    executeQuery(query, req, res, next)
+    database.executeQuery(query, (err, rows) => {
+        //Als de database een error verstuurd zal de error doorgegeven worden naar de gebruiker
+        if (err) {
+            const error = {
+                message: err,
+                code: 500
+            }
+            next(error)
+        }
+
+        //Als er geen error is worden de rijen getoont die uit de query volgen
+        if (rows) {
+            res.status(200).json({
+                result: rows
+            })
+        }
+    })
 })
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id',checkAuth,  (req, res, next) => {
+    logger.info('Handling PUT request to /api/appartments/:id')
 
     const apartmentId = req.params.id;
 
@@ -98,11 +180,35 @@ router.put('/:id', (req, res, next) => {
             "StreetAddress = '" + appartment.StreetAddress + "\'," +
             "PostalCode = '" + appartment.PostalCode + "\'," +
             "City = '" + appartment.City + "\' " +
-            "WHERE ApartmentId = " + apartmentId + ";")
+            "WHERE ApartmentId = " + apartmentId + 
+            " AND UserId = " + req.UserId +";")
 
         logger.info('query: ' + query)
 
-        executeQuery(query, req, res, next)
+        database.executeQuery(query, (err, rows) => {
+            //Als de database een error verstuurd zal de error doorgegeven worden naar de gebruiker
+            if (err) {
+                const error = {
+                    message: err,
+                    code: 500
+                }
+                next(error)
+            }
+    
+            //Als er geen error is worden de rijen getoont die uit de query volgen
+            if (rows) {
+                if (rows.rowsAffected < 1){
+                    res.status(401).json({
+                        message: 'You are not the owner of this appartment'
+                    })
+                } else{
+                    res.status(200).json({
+                    message: 'Your appartment has been updated!'
+                }) 
+                }
+               
+            }
+        })
 
     } catch (ex) {
         next(ex);
@@ -111,19 +217,15 @@ router.put('/:id', (req, res, next) => {
 })
 
 router.delete('/:id',checkAuth, (req, res, next) => {
+    logger.info('Handling DELETE request to /api/appartments/:id')
 
     const apartmentId = req.params.id;
 
     const query = ("DELETE FROM Apartment " +
-        "WHERE ApartmentId = " + apartmentId)
+    "WHERE ApartmentId = " + apartmentId + 
+    " AND UserId = " + req.UserId +";")
 
     logger.info('query: ' + query)
-
-    executeQuery(query, req, res, next)
-
-})
-
-function executeQuery(query, req, res, next){
 
     database.executeQuery(query, (err, rows) => {
         //Als de database een error verstuurd zal de error doorgegeven worden naar de gebruiker
@@ -137,12 +239,19 @@ function executeQuery(query, req, res, next){
 
         //Als er geen error is worden de rijen getoont die uit de query volgen
         if (rows) {
-            res.status(200).json({
-                result: rows
-            })
+            if (rows.rowsAffected < 1){
+                res.status(401).json({
+                    message: 'You are not the owner of this appartment'
+                })
+            } else{
+                res.status(200).json({
+                message: 'Your appartment has been deleted!'
+            }) 
+            }
+           
         }
     })
 
-}
+})
 
 module.exports = router;
